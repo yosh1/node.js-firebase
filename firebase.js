15612a193bf93609d6ed11f1519f07fc8e5b3993
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-29 00:03:32 +0900

    [Update] node.jsの雛形を追加

--- firebase.js
+++
@@ -1,7 +0,0 @@
-const admin = require('firebase-admin');
-const serviceAccount = require('js-firebase-tutorial-firebase-adminsdk-xy8c8-ff5d2a7d6d.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://js-firebase-tutorial.firebaseio.com'
-});
